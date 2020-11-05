899647419d5ec2e63951cac8e77de1fa3387cc36
ccallow <71406811+ccallow@users.noreply.github.com>
2020-11-05 13:09:05 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -36,11 +36,14 @@ def binary_to_decimal(binary):
     # binary is a tuple of length 8
     # return value is an integer decimal
     decimal = 0
+    print(binary)
+    """
     if binary[0] == 1:
         decimal = decimal + 128
     if binary[1] == 1:
-        decimal = value + 64
+        decimal = decimal + 64
     if binary[2] == 1:
+        print(binary[2])
         decimal = decimal + 32
     if binary[3] == 1:
         decimal = decimal + 16
@@ -52,13 +55,47 @@ def binary_to_decimal(binary):
         decimal = decimal + 2
     if binary[7] == 1:
         decimal = decimal + 1
-
+"""
     return decimal 
 
 def decimal_to_binary(decimal):
     # decimal is an integer value
     # binary is a tuple of length 8 that contains 1's and 0's
+    num = int(decimal)
+    value8 = 0
+    value7 = 0
+    value6 = 0
+    value5 = 0
+    value4 = 0
+    value3 = 0
+    value2 = 0
+    value1 = 0
 
+    if num >= 128:
+        value8 = 1
+        num = num - 128
+    if num >= 64:
+        value7 = 1
+        num = num - 64
+    if num >= 32:
+        value6 = 1
+        num = num - 32
+    if num >= 16:
+        value5 = 1
+        num = num - 16
+    if num >= 8:
+        value4 = 1
+        num = num - 8
+    if num >=4:
+        value3 = 1
+        num = num - 4
+    if num >= 2:
+        value2 = 1
+        num = num -2
+    if num >= 1:
+        value1 = 1
+        num = num - 1
+    binary = [value8, value7, value6, value5, value4, value3, value2, value1]
     return binary
 
 
@@ -66,10 +103,17 @@ def get_binary():
     # function should read the entry widget and generate an integer
     # this integer will be used as an input parameter for decimal to binary and the result updated
     # in the 8 checkboxes
-    entrybox.get
+    decimal = entryBox.get()
+    binary = decimal_to_binary(decimal)
+    state8.set(binary[0])
+    state7.set(binary[1])
+    state6.set(binary[2])
+    state5.set(binary[3])
+    state4.set(binary[4])
+    state3.set(binary[5])
+    state2.set(binary[6])
+    state1.set(binary[7])
 
-
-    binary = binary_to_decimal(decimal)
 
 
 def get_decimal():
@@ -77,14 +121,14 @@ def get_decimal():
     # this tuple will be used as an input parameter for binary_to_decimal and the result updated
     # in the entry box
     binary = []
-    binary.append(w8.get())
-    binary.append(w7.get())
-    binary.append(w6.get())
-    binary.append(w5.get())
-    binary.append(w4.get())
-    binary.append(w3.get())
-    binary.append(w2.get())
-    binary.append(w1.get())
+    binary.append(state8.get())
+    binary.append(state7.get())
+    binary.append(state6.get())
+    binary.append(state5.get())
+    binary.append(state4.get())
+    binary.append(state3.get())
+    binary.append(state2.get())
+    binary.append(state1.get())
    
     decimal = binary_to_decimal(binary)
     blank.set(decimal)
@@ -92,14 +136,14 @@ def get_decimal():
 blank = StringVar()
 blank.set("")
 
-w1 = Checkbutton (win, variable = state1)
-w2 = Checkbutton (win, variable = state2)
-w3 = Checkbutton (win, variable = state3)
-w4 = Checkbutton (win, variable = state4)
-w5 = Checkbutton (win, variable = state5)
-w6 = Checkbutton (win, variable = state6)
-w7 = Checkbutton (win, variable = state7)
-w8 = Checkbutton (win, variable = state8)
+w1 = Checkbutton (win, variable = state8)
+w2 = Checkbutton (win, variable = state7)
+w3 = Checkbutton (win, variable = state6)
+w4 = Checkbutton (win, variable = state5)
+w5 = Checkbutton (win, variable = state4)
+w6 = Checkbutton (win, variable = state3)
+w7 = Checkbutton (win, variable = state2)
+w8 = Checkbutton (win, variable = state1)
 entryBox = Entry (win, textvariable = blank)
 b1 = Button(win, text="Convert to Binary", command=get_binary)
 b2 = Button(win, text="Convert to Decimal", command=get_decimal)
