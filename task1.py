1b8410402f9d2c3e9115593a2d839669ec3b9b49
ccallow <71406811+ccallow@users.noreply.github.com>
2020-11-05 12:08:48 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -18,7 +18,8 @@ Use assignment_test.py to test your functions
 
 
 import tkinter as tk 
-
+titleLabel = Label(win, text = "Binary/Decimal Converter!")
+titleLabel.config(text = '50')
 
 
 def binary_to_decimal(binary):
@@ -39,6 +40,8 @@ def get_binary():
     # function should read the entry widget and generate an integer
     # this integer will be used as an input parameter for decimal to binary and the result updated
     # in the 8 checkboxes
+    entrybox.get
+
 
     binary = binary_to_decimal(decimal)
 
@@ -54,10 +57,34 @@ def get_decimal():
 
 win = tk.Tk()
 
+blank = StringVar()
+blank.set("")
 
-
+w1 = Checkbutton ( win )
+w2 = Checkbutton ( win )
+w3 = Checkbutton ( win )
+w4 = Checkbutton ( win )
+w5 = Checkbutton ( win )
+w6 = Checkbutton ( win )
+w7 = Checkbutton ( win )
+w8 = Checkbutton ( win )
+entryBox = Entry(win, textvariable = blank)
 b1 = Button(win, text="Convert to Binary", command=get_binary)
 b2 = Button(win, text="Convert to Decimal", command=get_decimal)
+
+#Position of Widgets
+titleLabel.Grid(row = 1, column = 2, columnspan = 6)
+w1.grid(row = 2, column = 1)
+w2.grid(row = 2, column = 2)
+w3.grid(row = 2, column = 3)
+w4.grid(row = 2, column = 4)
+w5.grid(row = 2, column = 5)
+w6.grid(row = 2, column = 6)
+w7.grid(row = 2, column = 7)
+w8.grid(row = 2, column = 8)
+b1.grid(row = 3, column = 1, columnspan = 4)
+b2.grid(row = 3, column = 5, columnspan = 4)
+entrybox.grid(row = 4, column = 2, columnspan = 6)
 
 
 
